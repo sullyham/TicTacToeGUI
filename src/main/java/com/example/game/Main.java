7f5aa46db66a5548e23300aba8dcdf22b9027ec4
Sully Ham <sulaymanhumzamc@gmail.com>
2026-02-05 17:33:30 -0500

    Made stage start from playscreen, and working localgame scene swap!

--- src/main/java/com/example/game/Main.java
+++ src/main/java/com/example/game/Main.java
@@ -14,28 +14,28 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
-    Label wincond;
 
     static void main(String[] args) {
         launch(args);
     }
     //DAMN
+    public static void swapLocal(Stage stage){
+        TicTacToeConsole gamer = new TicTacToeConsole();
+        BorderPane pane = new BorderPane();
+        GridPane pane2 = gamer.returnPane();
+        Scene game = MakeTools.makeScene(pane, "stylesheet.css");
+        pane.setCenter(pane2); //TicTacToe Board
+        pane.setTop(gamer.returnLabel()); //Win Condition
+        stage.setScene(game);
+    }
     @Override
     public void start(Stage stage) throws Exception {
         //The top that shows who wins!
         //Calling my game class
-
-//        TicTacToeConsole gamer = new TicTacToeConsole();
-//        BorderPane pane = new BorderPane();
-//        GridPane pane2 = gamer.returnPane();
-//        Scene game = MakeTools.makeScene(pane, "stylesheet.css");
-//        pane.setCenter(pane2); //TicTacToe Board
-//        pane.setTop(gamer.returnLabel()); //Win Condition
-
         PlayScreen screen = new PlayScreen();
         BorderPane playboard = new BorderPane();
         VBox buttons = screen.returnPlayScreen();
-        Scene playscreen = MakeTools.makeScene(playboard, "stylesheet.css");
+        Scene playscreen = MakeTools.makeScene(playboard, "play_stylesheet.css");
         playboard.setCenter(buttons);
         buttons.setAlignment(Pos.CENTER);
         stage.getIcons().add(new Image("instagram.png"));
--- src/main/java/com/example/game/PlayScreen.java
+++ src/main/java/com/example/game/PlayScreen.java
@@ -1,23 +1,34 @@
 package com.example.game;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class PlayScreen {
     private VBox Play;
     public PlayScreen(){
         Play = new VBox();
+        Label title = new Label("TIC-TAC-TOE");
         Play.setSpacing(30);
         Button playlocal = new Button("Local Game");
+        playlocal.setMinWidth(150);
         Button playmulti = new Button("LAN Game");
-        Play.getChildren().addAll(playlocal,playmulti);
+        playlocal.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Node source = (Node) actionEvent.getSource();
+                Stage stage = (Stage) source.getScene().getWindow();
+                Main.swapLocal(stage);
+            }
+        });
+        playmulti.setMinWidth(150);
+        Play.getChildren().addAll(title,playlocal,playmulti);
     }
-
-
-
-
-
     public VBox returnPlayScreen(){
         return Play;
     }
